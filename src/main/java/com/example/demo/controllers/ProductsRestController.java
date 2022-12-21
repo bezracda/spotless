d409a61c9527d617c28f2f02a2c472f672823fe0
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2022-12-21 16:16:18 +0300

    Swagger operation descriptions added

--- src/main/java/com/example/demo/controllers/ProductsRestController.java
+++ src/main/java/com/example/demo/controllers/ProductsRestController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import com.example.demo.exception_handling.NoSuchProductException;
 import com.example.demo.models.Product;
 import com.example.demo.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,7 @@ public class ProductsRestController {
     private final ProductService productService;
 
     @GetMapping("/")
+    @Operation(summary = "Возвращает список всех товаров")
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = productService.getAllProducts();
         if ((products == null) || (products.isEmpty())) {
@@ -27,6 +30,7 @@ public class ProductsRestController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Возвращает товар по id")
     public ResponseEntity<Product> getProduct(@PathVariable Long id) {
         Product product = productService.findProductById(id);
         if (product == null) {
@@ -36,12 +40,14 @@ public class ProductsRestController {
     }
 
     @PostMapping("/")
+    @Operation(summary = "Создает новый товар")
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
         productService.addProduct(product);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Изменяет товар с указанным id")
     public ResponseEntity<Product> updateProduct(@RequestBody Product product, @PathVariable Long id) {
         if (productService.findProductById(id) == null) {
             throw new NoSuchProductException("There is no product with ID = " + id + " in Database");
@@ -52,6 +58,7 @@ public class ProductsRestController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаляет товар с указанным id")
     public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
         if (productService.findProductById(id) == null) {
             throw new NoSuchProductException("There is no product with ID = " + id + " in Database");
